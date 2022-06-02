8381d8ba6ff2b75a3268d542f40c38b0e07c21e4
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2022-06-02 03:50:12 +0000

    feat(autoReonnect): add an error listener

--- src/utils/auto-reconnect.ts
+++ src/utils/auto-reconnect.ts
@@ -1,15 +1,22 @@
 import { ExtraWebSocket } from '@src/extra-websocket'
 import { delay } from 'extra-promise'
-import { isntUndefined } from '@blackglory/prelude'
+import { pass, isntUndefined } from '@blackglory/prelude'
 import { AbortController } from 'extra-abort'
 
 export function autoReconnect(ws: ExtraWebSocket, timeout?: number): () => void {
   const controller = new AbortController()
 
+  // Make sure the error listener is added, prevent crashes due to uncaught errors.
+  ws.addEventListener('error', ignore)
   ws.addEventListener('close', listener)
   return () => {
     controller.abort()
     ws.removeEventListener('close', listener)
+    ws.removeEventListener('error', ignore)
+  }
+
+  function ignore() {
+    pass()
   }
 
   async function listener(): Promise<void> {
